60ee775695fc561a97f2d1d23642b8330d6a712e
Lennie Nurse <lnursej@gmail.com>
2024-09-13 19:56:00 -0400

    not finish doing set

--- data-types.py
+++ data-types.py
@@ -2,26 +2,40 @@
 planeteers = ["Earth", "Wind", "Captain Planet", "Fire", "Water"]
 
 # Access the second value in `planeteers`
+print(planeteers[1])
 
 
 # Add "Heart" to the end of `planeteers`
+planeteers.append("heart")
+print(planeteers)
 
-
+# or 
+# this Creates a new list
+# planeteers_new = planeteers + ["hearts"]
+# print(planeteers_new)
+      
 # Remove "Captain Planet" from the list
-
+planeteers.remove("Captain Planet")
 
 # Combine `planeteers` with `rangers = ["Red", "Blue", "Pink", "Yellow", "Black"]` and save the result in a variable called `heroes`
+rangers = ["Red", "Blue", "Pink", "Yellow", "Black"]
+# planeteers.append(rangers) 
+heroes = planeteers + rangers
+print(heroes)
 
 
 # Alphabetize the contents of `heroes` using a function
+def alphabetize(heroes):
+    return heroes.upper()
 
 
 # Randomize the contents of `heroes` using a function. 
 # The Python documentation might help: https://docs.python.org/2/library/random.html
-
+heroes.random()
 
 # Bonus: Select a random element from `heroes` using a function
 # The Python documentation might help: https://docs.python.org/2/library/random.html
+
 
 
 # Initialize a dictionary called `ninja_turtle` with the properties `name`, `weapon` and `radical`
